935c360d91cacaf9f0eec57e56789231ab3d0873
Sarthak-Oza <sarthak0499@outlook.com>
2023-11-01 22:42:15 +0000

    added os to clear window

--- mini-projects/age_quiz.py
+++ mini-projects/age_quiz.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
-import numpy
+from numpy import average
 from colorama import Fore, Back, Style
+from os import system
 from questions import questions
+
+CHOICE_MIN = 1
+CHOICE_MAX = 5
 
 def ask_question(question, options):
     while True:
@@ -12,7 +16,7 @@ def ask_question(question, options):
                 print(f"{option}. {option_description}")
 
             print()
-            choice = int(input(Back.GREEN + "Enter the number of your choice from above (1-5):"  + Style.RESET_ALL + " "))
+            choice = int(input(Back.GREEN + f"Enter the number of your choice from {CHOICE_MIN}-{CHOICE_MAX}:" + Style.RESET_ALL + " "))
             print()
 
             if choice in options:
@@ -23,10 +27,11 @@ def ask_question(question, options):
                 print(f"{Fore.RED}\n######## Invalid input. Please try again, enter a valid option (1-5). ########{Style.RESET_ALL}")
 
 def determine_age(answers):
-    calculated_age = int(numpy.average(list(answer * 15 for answer in answers)))
+    calculated_age = int(average(list(answer * 15 for answer in answers)))
     return calculated_age
 
 def main():
+    system("clear")
     print(f"{Fore.YELLOW}{Back.CYAN}{'#' * 19} Welcome to the 'What Age Are You?' Quiz! {'#' * 19}{Style.RESET_ALL}\n")
     print(f"{Fore.YELLOW}{Back.CYAN}{'#' * 8} Answer the following questions to find out your age mentality. {'#' * 8}{Style.RESET_ALL}\n")
 
@@ -36,13 +41,11 @@ def main():
     for question, options in questions.items():
         choice = ask_question(question, options)
         answers.append(choice)
+        system("clear")
 
     result = determine_age(answers)
-    print(f"{Fore.YELLOW}{Back.BLUE}************  You are {Style.BRIGHT}{result}{Style.NORMAL} years old!  ************{Style.RESET_ALL}")
+    print(f"{Fore.YELLOW}{Back.BLUE}************  You are {Style.BRIGHT}{result}{Style.NORMAL} years old!  ************{Style.RESET_ALL}", end="\n\n")
 
 
 if __name__ == "__main__":
     main()
-
-
-
